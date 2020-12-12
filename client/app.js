a59b654e82b8c80bf4ca7c7f021b5557d565ed54
Marcelina <m.oscislowska@gmail.com>
2020-12-12 16:47:51 +0100

    Add login function

--- client/app.js
+++ client/app.js
@@ -6,3 +6,22 @@ const userNameInput = document.querySelector('username');;
 const messageContentInput = document.getElementById('message-contentconst');
 
 
+let userName;
+
+
+function login(event) {
+    event.preventDefault();
+
+    if (userNameInput.value !== '') {
+      userName = userNameInput.value;
+      loginForm.classList.remove('show');
+      messagesSection.classList.add('show');
+    } 
+    else {
+      alert("Name can't be left blank!");
+    }
+}
+
+loginForm.addEventListener('submit', login);
+
+
