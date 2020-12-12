a3ba1d7da268d7401db59b7b13c5eb62993f1a88
Marcelina <m.oscislowska@gmail.com>
2020-12-12 16:46:18 +0100

    Add references

---
+++ client/app.js
@@ -0,0 +1,8 @@
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.querySelector('username');;
+const messageContentInput = document.getElementById('message-contentconst');
+
+
